2eba5312fb80ccdf6663011c9e58bd64aafa9d35
Ryan Chang <rchang@videoamp.com>
2019-10-26 23:03:04 -0700

    stuff

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,11 +3,9 @@ import { Link } from 'gatsby'
 
 import Layout from 'components/layout'
 import Image from 'components/image'
-import SEO from 'components/seo'
 
 const IndexPage = () => (
   <Layout>
-    <SEO title='Home' />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
@@ -15,6 +13,7 @@ const IndexPage = () => (
       <Image />
     </div>
     <Link to='/page-2/'>Go to page 2</Link>
+    <Link to='/order-return-status'>EasyPost Order Return Status</Link>
   </Layout>
 )
 
